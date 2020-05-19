cbcdd77515000237b226250e28fcc9a25b3c7a90
Andrew <andrewapinon@gmail.com>
2020-05-19 11:05:26 +0800

    added cors

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const databaseRoute = require("./Routes/Database");
+const cors = require("cors")
 
 const port = process.env.PORT || 3000;
 
@@ -9,6 +10,7 @@ const port = process.env.PORT || 3000;
 
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
+app.use(cors())
 
 app.listen(port, ()=>{
     console.log(`server started on ${port}`);
