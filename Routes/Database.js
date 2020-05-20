e45f91f656101b34477c95fe1d7fe6f77fb76374
Andrew <andrewapinon@gmail.com>
2020-05-20 12:54:46 +0800

    changed res.paginatedResults = results.results

--- Routes/Database.js
+++ Routes/Database.js
@@ -29,7 +29,7 @@ function paginatedResults(model) {
         }
         try{
             results.results = await model.find().limit(limit).skip(startIndex).exec()
-            res.paginatedResults = results
+            res.paginatedResults = results.results
             next()
         }catch (e){
             res.status(500).json({message: e.message})
