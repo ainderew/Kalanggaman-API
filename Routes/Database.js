18d390fb4d00a8242c7e06082d39b851dd19818a
Andrew <andrewapinon@gmail.com>
2020-05-20 19:39:07 +0800

    added page information to json

--- Routes/Database.js
+++ Routes/Database.js
@@ -29,7 +29,7 @@ function paginatedResults(model) {
         }
         try{
             results.results = await model.find().limit(limit).skip(startIndex).exec()
-            res.paginatedResults = results.results
+            res.paginatedResults = results
             next()
         }catch (e){
             res.status(500).json({message: e.message})
