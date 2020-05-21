36e9012c74387235b41514abaabd9f6b931cf97b
Andrew <andrewapinon@gmail.com>
2020-05-21 22:09:42 +0800

    line 25 commented out redirecting disabled

--- Routes/FormSubmit.js
+++ Routes/FormSubmit.js
@@ -22,7 +22,7 @@ router.post("/", async (req,res)=> {
         phoneNumber: req.body.phoneNumber,
         date: req.body.name
     })
-    // res.status(301).redirect("https://serene-hypatia-9cba55.netlify.app/database");
+    res.status(301).redirect("https://serene-hypatia-9cba55.netlify.app/database");
     post.save();    
 })
 
