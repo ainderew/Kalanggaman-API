45e1ab866b8be7e74f4e6a226d7fa0ec9eb81d8e
Andrew <andrewapinon@gmail.com>
2020-05-22 23:05:31 +0800

    added search data only on filtered by current month route (findSpecificFilteredByMonth)

--- Routes/Database.js
+++ Routes/Database.js
@@ -29,7 +29,7 @@ router.get("/currentMonthFilter",currentMonthFilter(), async (req,res) =>{
 
 })
 router.get("/findSpecificFilteredByMonth",searchResultsFilteredByMonth(GuestReservation), async (req,res) =>{
-    res.json(res.response)
+    res.json(res.searchResults)
     
 
 })
