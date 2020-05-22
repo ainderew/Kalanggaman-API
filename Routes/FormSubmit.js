6d6dd080f34585d013bffca6124b0f4bf2813d65
Andrew <andrewapinon@gmail.com>
2020-05-22 10:23:55 +0800

    added search middleware in database.js

--- Routes/FormSubmit.js
+++ Routes/FormSubmit.js
@@ -10,17 +10,17 @@ router.post("/", async (req,res)=> {
     let tempYearHolder ="";
     let tempMonthHolder ="";
     
-    formatedDate = req.body.reservationDate.split("-")
-    tempDayHolder = formatedDate[2].split("T")[0];
-    tempYearHolder = formatedDate[0];
-    tempMonthHolder = formatedDate[1]
-    formatedDate = `${tempMonthHolder}/${tempDayHolder}/${tempYearHolder}`
+    // formatedDate = req.body.reservationDate.split("-")
+    // tempDayHolder = formatedDate[2].split("T")[0];
+    // tempYearHolder = formatedDate[0];
+    // tempMonthHolder = formatedDate[1]
+    // formatedDate = `${tempMonthHolder}/${tempDayHolder}/${tempYearHolder}`
 
     const post = new model({
         name: req.body.name,
         email: req.body.email,
         phoneNumber: req.body.phoneNumber,
-        date: formatedDate
+        date: req.body.reservationDate
     })
     res.status(301).redirect("https://serene-hypatia-9cba55.netlify.app/database");
     post.save();    
