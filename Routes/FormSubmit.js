68b41cea4b698eadce5eea3110418282f74c6e93
Andrew <andrewapinon@gmail.com>
2020-05-22 09:11:06 +0800

    added search middleware in database.js

--- Routes/FormSubmit.js
+++ Routes/FormSubmit.js
@@ -4,6 +4,18 @@ const model = require("../Models/GuestReservation");
 
 router.post("/", async (req,res)=> {
     
+
+    let formatedDate =""
+    let tempDayHolder ="";
+    let tempYearHolder ="";
+    let tempMonthHolder ="";
+    
+    formatedDate = req.body.reservationDate.split("-")
+    tempDayHolder = formatedDate[2].split("T")[0];
+    tempYearHolder = formatedDate[0];
+    tempMonthHolder = formatedDate[1]
+    formatedDate = `${tempMonthHolder}/${tempDayHolder}/${tempYearHolder}`
+
     const post = new model({
         name: req.body.name,
         email: req.body.email,
